c7e1a31a89e0a7b60c9bb0b1dcb99647d6cf00fd
John O'Connor <oconnojb@gmail.com>
2018-07-28 20:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Code your solution in this file.
 function lowerCaseDrivers(col) {
-  const newCol = col.map(function (dr) {
+  return col.map(function (dr) {
     return dr.toLowerCase();
   })
-  return newCol;
 }
 
 function nameToAttributes(col) {
@@ -11,7 +10,6 @@ function nameToAttributes(col) {
     let drArr = dr.split(' ');
     return {firstName: drArr[0], lastName: drArr[1]}
   })
-  //return newCol;
 }
 
 function attributesToPhrase(col) {
