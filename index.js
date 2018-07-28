bf235d05565cf020fe789bcdc9bc9da7111ed4fc
John O'Connor <oconnojb@gmail.com>
2018-07-28 20:14:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(col) {
+  return col.map(function (dr) {
+    return dr.toLowerCase();
+  })
+}
+
+function nameToAttributes(col) {
+  return col.map(function (dr) {
+    let drArr = dr.split(' ');
+    return {firstName: drArr[0], lastName: drArr[1]}
+  })
+}
+//{ name: 'Bobby',   hometown: 'Pittsburgh'  }
+function attributesToPhrase(col) {
+  return col.map(function (dr) {
+    return dr['name'] + ' is from ' + dr['hometown'];
+  })
+}
